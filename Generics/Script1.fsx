ab8c4d3a68ab70fb9ce5a9125e39885c60c7cfd1
Ernesto Rodriguez <neto@netowork.me>
2015-01-19 12:19:42 -0800

    Simple implementation with generics and subtypeing. Sart of use of reflection

---
+++ Generics/Script1.fsx
@@ -0,0 +1,64 @@
+﻿type Kaiser = Toro of int | Rico of string*int
+
+type List<'t> = Cons of 't*List<'t> | SemiCons of 't | Nil
+
+type Microsoft.FSharp.Reflection.UnionCaseInfo with
+    member x.Constructor with get() = 
+        x.DeclaringType.GetMethods() 
+        |> Array.find (fun mi -> mi.Name = sprintf "New%s" x.Name || mi.Name = sprintf "get_%s" x.Name)
+    member x.Matcher with get() =
+        x.DeclaringType.GetMethod(sprintf "get_Is%s" x.Name)
+
+let constrs = Microsoft.FSharp.Reflection.FSharpType.GetUnionCases typeof<List<Kaiser>>
+
+constrs.[0].DeclaringType.GetNestedType(constrs.[0].Name).GetMethods() |> Array.filter (fun mi -> mi.Name.Contains "get_Item")
+
+constrs.[0].DeclaringType.GetMethods() |> Array.filter (fun mi -> mi.Name.Contains "Nil")
+
+typeof<int>
+//constrs.[0].Constructor.[0].DeclaringType.GetMethod("NewCons").GetParameters() // .MemberType
+
+type TypeConstructor<'t> = {
+
+        Matcher : 't -> bool
+        Invoke : array<obj> -> 't
+        Elems : 't -> (obj*System.Type) [] option
+        Params : System.Type []
+    }
+
+open Microsoft.FSharp.Reflection
+
+let makeTypeConstructor<'t> (c : UnionCaseInfo) =
+    let constr = c.Constructor
+    let invoke args = constr.Invoke(null,args) :?> 't
+    let matcher (o : 't) = c.Matcher.Invoke(o :> obj, [||]) :?> bool
+    let getters =
+        if c.DeclaringType.GetNestedType(c.Name) = null then
+            [||]
+        else
+            let ct = 
+                if c.DeclaringType.GetNestedType(c.Name).ContainsGenericParameters then
+                    c.DeclaringType.GetNestedType(c.Name).MakeGenericType c.DeclaringType.GenericTypeArguments
+                else
+                    c.DeclaringType.GetNestedType(c.Name)
+            ct.GetMethods() |> Array.filter (fun mi -> mi.Name.Contains "get_Item")
+        
+    let elems (o : 't) =
+        if matcher o then
+            getters |> Array.map (fun g -> g.Invoke(o,[||])) 
+                    |> Array.zip (c.GetFields() |> Array.map (fun fi -> fi.PropertyType)) 
+                    |> Array.map (fun (a,b) -> (b,a)) |> Some
+        else
+            None
+    {
+        Matcher = matcher
+        Invoke = invoke
+        Elems = elems
+        Params = [||]    
+    }
+
+constrs.[2].DeclaringType.GetNestedTypes() |> Array.map (fun t -> t.Name) //.Constructor.Invoke(null,[||])
+MakeTypeConstructor<List<Kaiser>>(constrs.[2]).Elems (SemiCons (Toro 5) : List<Kaiser>) //.Elems (Cons (Toro 6,Nil))
+
+\DeclaringType.Module.GetTypes() |> Array.map (fun
+ mi -> mi.Name)
--- Generics/uniplate.fs
+++ Generics/uniplate.fs
@@ -1,274 +1,263 @@
-module Uniplate
+namespace Generics
 
-[<AbstractClass>]
-type Rep<'t>() =
-  class
+open System
+open Microsoft.FSharp.Reflection
 
-    member o.elem x =
-      match x with
-        | :? 't -> true
-  end
+module Uniplate =
+    let a = id
 
-  
+module Reflection =
 
-let (|INT|_|) ((e : Rep<'t>),o) =
-  match o with
-    | :? 't as e' -> Some e'
-    | _ -> None
+    [<CustomComparison>]
+    type TypeConstructor<'t> = {
 
-// let (|Data|_|) (e : Rep) =
-//   match e with
-//     | INT _ -> None
-//     | _ -> Some e
+        Info : UnionCaseInfo
+        Matcher : 't -> bool
+        Invoke : array<obj> -> 't
+        Elems : 't -> (obj*System.Type) [] option
+        Params : System.Type []
+        }
+        with 
+            interface IComparable with
+                override o.CompareTo(o' : obj) = 
+                    match o' with
+                        | :? TypeConstructor<'t> as o'' -> 
+                            compare (o.Info.Name,o.Info.Tag) (o''.Info.Name,o''.Info.Tag)
+                        | _ -> 1
 
+    type Microsoft.FSharp.Reflection.UnionCaseInfo with
+        member x.Constructor with get() = 
+            x.DeclaringType.GetMethods() 
+            |> Array.find (fun mi -> mi.Name = sprintf "New%s" x.Name || mi.Name = sprintf "get_%s" x.Name)
+        member x.Matcher with get() =
+            x.DeclaringType.GetMethod(sprintf "get_Is%s" x.Name)
 
-[<AbstractClass>]
-type Rep() =
-  class
-  end
-
-type RInt() =
-  class
-    inherit Rep()
-  end
-  
-
-type Data<'t>() =
-  class
-    inherit Rep()
-    abstract GetConstr : 't -> Rep*obj
-  end
-
-[<AbstractClass>]
-type Meta() =
-  class
-  end
-
-[<AbstractClass>]
-type Constr<'t>() =
-  class
-    inherit Meta()
-  end
-
-[<AbstractClass>]
-type L<'t>() =
-  class
-    inherit Constr<'t>()
-    abstract Constr : Constr<'t> -> Constr<'t>
-  end
-
-[<AbstractClass>]
-type R<'t>() =
-  class
-    inherit Constr<'t>()
-    abstract Constr : Constr<'t> -> Constr<'t>
-  end
-
-[<AbstractClass>]
-type Prod<'t>() =
-  class
-    inherit Constr<'t>()
-    abstract Prod : Constr<'t>*Constr<'t> -> Prod<'t>
-  end
-
-[<AbstractClass>]
-type Id<'t>() =
-  class
-    inherit Constr<'t>()
-    abstract Init : Constr<'t> -> Id<'t>
-  end
-
-[<AbstractClass>]
-type K<'v,'t>() =
-  class
-    inherit Constr<'t>()
-    abstract Init : 'v -> K<'v,'t>
-  end
-
-[<AbstractClass>]
-type U<'t>() =
-  class
-    inherit Constr<'t>()
-    abstract Create : unit -> U<'t>
-  end
-
-type D() =
-  class
-    inherit Meta()
-  end
-
-(*
-[<AbstractClass>]
-type GSumR<'t>() =
-  class
-
-//    member o.gSum(meta:K, i:int) = i
-
-    member o.gSum(meta:L, r:obj) =
-      o.gSum(r)
-
-    member o.gSum(meta:R, r:obj) =
-      o.gSum(r)
-
-    member o.gSum(meta:Prod, i:int, r:obj) =
-      i + o.gSum(r)
-
-    member o.gSum(meta:Id, r:'t) =
-      o.gSum(r)
-
-    member o.gSum(meta:U, ()) = 0
-
-    member o.gSum(meta:D, x:obj) = 0
-
-    // member o.gSum(r:Data<'t>,v:'t) =
-    //   let (r',v') = r.GetConstr(v)
-    //   match r' with
-    //     :? RInt as r'' -> o.gSum(r'',v' :?> int)
-
-    abstract gSum : obj -> int
-  end
-*)
-[<AbstractClass>]
-type Everywhere<'t>() =
-
-  member o.Everywhere(meta:L<'t>, r:obj,f:int -> int) =
-    o.Everywhere(r,f) |> meta.Constr
-
-  member o.Everywhere(meta:K<int,'t>, i:int,f:int -> int) =
-    f i |> meta.Init
-
-  member o.Everywhere(meta:Prod<'t>, i:obj, r:obj,f:int -> int) =
-    meta.Prod(o.Everywhere(i,f),o.Everywhere(r,f))
-
-  member o.Everywhere(meta:R<'t>, r:obj,f:int -> int) =
-    o.Everywhere(r,f) |> meta.Constr
-
-  member o.Everywhere(meta:U<'t>, u : unit, f:int->int) =
-    meta
-
-  member o.Everywhere(meta:Id<'t>, r:'t, f : int -> int) =
-    o.Everywhere(r,f) |> meta.Init
-
-  abstract Everywhere : obj*(int -> int) -> Constr<'t>
-  
-type AList<'t> = Cons of 't*AList<'t> | Nil
-
-type AListP<'c> =
-  {
-    list : AList<int>
-    meta : 'c
-   }
-
-type LConstr() =
-  class
-    inherit L<AList<int>>()
-    let mutable (elem : Option<Constr<AList<int>>>) = None
-    override o.Constr(e) =
-      elem <- Some e
-      o :> _
-  end
-
-type RConstr() =
-  class
-    inherit R<AList<int>>()
-    let mutable (elem : Option<Constr<AList<int>>>) = None
-    override o.Constr(e) =
-      elem <- Some e
-      o :> _
-  end
-
-type ProdConstr() =
-  class
-    inherit Prod<AList<int>>()
-    let mutable (elem : Option<Constr<AList<int>>*Constr<AList<int>>>) = None
-    override o.Prod(c1,c2) =
-      elem <- Some(c1,c2)
-      o :> _
-  end
+    let makeTypeConstructor<'t> (c : UnionCaseInfo) =
+        let constr = c.Constructor
+        let invoke args = constr.Invoke(null,args) :?> 't
+        let matcher (o : 't) = c.Matcher.Invoke(o :> obj, [||]) :?> bool
+        let getters =
+            if c.DeclaringType.GetNestedType(c.Name) = null then
+                [||]
+            else
+                let ct = 
+                    if c.DeclaringType.GetNestedType(c.Name).ContainsGenericParameters then
+                        c.DeclaringType.GetNestedType(c.Name).MakeGenericType c.DeclaringType.GenericTypeArguments
+                    else
+                        c.DeclaringType.GetNestedType(c.Name)
+                ct.GetMethods() |> Array.filter (fun mi -> mi.Name.Contains "get_Item")
+            
+        let elems (o : 't) =
+            if matcher o then
+                getters |> Array.map (fun g -> g.Invoke(o,[||])) 
+                        |> Array.zip (c.GetFields() |> Array.map (fun fi -> fi.PropertyType)) 
+                        |> Array.map (fun (a,b) -> (b,a)) |> Some
+            else
+                None
+        {
+            Info = c
+            Matcher = matcher
+            Invoke = invoke
+            Elems = elems
+            Params = [||]    
+        }
 
 
-type KConstr() =
-  class
-    inherit K<int,AList<int>>()
-    let mutable (v : Option<int>) = None
-    override o.Init(x) =
-      v <- Some x
-      o :> _
-  end
+module Rep =
 
-type UConstr() =
-  class
-    inherit U<AList<int>>()
-    override o.Create() = UConstr() :> _
-  end
+    type Microsoft.FSharp.Reflection.UnionCaseInfo with
+        member x.Constructor with get() = x.DeclaringType.GetMethod(sprintf "New%s" x.Name)
 
-type IdConstr() =
-  class
-    inherit Id<AList<int>>()
-    let mutable (elem : Option<Constr<AList<int>>>) = None
-    override o.Init(e) =
-      elem <- Some e
-      o :> _
-  end
+    [<AbstractClass>]
+    type Meta() =
+      class
+      end
     
+    [<AbstractClass>]
+    type Constr<'t>() =
+      class
+        inherit Meta()
+      end
+    
+    [<AbstractClass>]
+    type SumConstr<'t>() =
+        class
+            inherit Constr<'t>()
+            abstract Elem : Constr<'t> with get
+        end
 
-type EverywhereImp() =
-  class
-    inherit Everywhere<AList<int>>()
-
-    override o.Everywhere(e:obj,f:int->int) =
-      match e with
-        | :? AList<int> as e' ->
-          match e' with
-            | Cons(_) -> o.Everywhere({list = e';meta = LConstr()},f)
-            | Nil -> o.Everywhere({list = e';meta = RConstr()},f)
-        | :? AListP<LConstr> as e' ->
-          match e'.list with
-            | Cons (x,xs) ->
-              let (c : L<AList<int>>) = LConstr() :> _
-              o.Everywhere(c,{list=Cons(x,xs);meta=ProdConstr()},f)
-        | :? AListP<ProdConstr> as e' ->
-          match e'.list with
-            | Cons (x,xs) ->
-              let (c : Prod<AList<int>>) = ProdConstr() :> _
-              o.Everywhere(c,{list=Cons(x,xs);meta=KConstr()},{list=Cons(x,xs);meta=IdConstr()},f) :> _
-        | :? AListP<KConstr> as e' ->
-          match e'.list with
-            | Cons (x,_) -> o.Everywhere(KConstr(),x,f) :> _
-        | :? AListP<IdConstr> as e' ->
-          match e'.list with
-            | Cons (_,xs) -> o.Everywhere(IdConstr(),xs,f) :> _
-            | Nil ->
-              let (c : U<AList<int>>) = UConstr() :> _
-              o.Everywhere(c,(),f) :> _
-        | :? AListP<RConstr> as e' -> o.Everywhere(RConstr(),{list=Nil;meta=IdConstr()},f)
-        | :? AListP<UConstr> -> o.Everywhere(UConstr(),(),f) :> _
+    type L<'t>(elem : Constr<'t>) =
+      class
+        inherit SumConstr<'t>()
+        override o.Elem with get() = elem
+      end
+    
+    type R<'t>(elem : Constr<'t>) =
+      class
+        inherit SumConstr<'t>()
+        override o.Elem with get() = elem
+      end
+    
+    type Prod<'t>(elem : Constr<'t>*Constr<'t>) =
+      class
+        inherit Constr<'t>()
+        member o.Elem with get() = elem
+      end
+    
+    type Id<'t>(elem : 't) =
+      class
+        inherit Constr<'t>()
+        member o.Elem with get() = elem
+      end
+    
+    type K<'v,'t>(elem : 'v) =
+      class
+        inherit Constr<'t>()
+        member o.Elem with get() = elem
+      end
+    
+    type U<'t>() =
+      class
+        inherit Constr<'t>()
+      end
+    
+    type D() =
+      class
+        inherit Meta()
+      end
+    
+    (*
+    [<AbstractClass>]
+    type GSumR<'t>() =
+      class
+    
+    //    member o.gSum(meta:K, i:int) = i
+    
+        member o.gSum(meta:L, r:obj) =
+          o.gSum(r)
+    
+        member o.gSum(meta:R, r:obj) =
+          o.gSum(r)
+    
+        member o.gSum(meta:Prod, i:int, r:obj) =
+          i + o.gSum(r)
+    
+        member o.gSum(meta:Id, r:'t) =
+          o.gSum(r)
+    
+        member o.gSum(meta:U, ()) = 0
+    
+        member o.gSum(meta:D, x:obj) = 0
+    
+        // member o.gSum(r:Data<'t>,v:'t) =
+        //   let (r',v') = r.GetConstr(v)
+        //   match r' with
+        //     :? RInt as r'' -> o.gSum(r'',v' :?> int)
+    
+        abstract gSum : obj -> int
+      end
+    *)
+    [<AbstractClass>]
+    type Everywhere<'c>() =
+    
+      member o.Everywhere(meta:L<'t>, r:obj,f:int -> int) =
+        L(o.Everywhere(r,f))
+    
+      member o.Everywhere(meta:K<int,'t>, i:int,f:int -> int) =
+        K(f i)
+    
+      member o.Everywhere(meta:Prod<'t>, i:obj, r:obj,f:int -> int) =
+        Prod(o.Everywhere(i,f),o.Everywhere(r,f))
+    
+      member o.Everywhere(meta:R<'t>, r:obj,f:int -> int) =
+        R(o.Everywhere(r,f))
+    
+      member o.Everywhere(meta:U<'t>, u : unit, f:int->int) =
+        meta
+    
+      member o.Everywhere(meta:Id<'t>, r:'t, f : int -> int) =
+        Id(o.To(o.Everywhere(r,f)))
+    
+      abstract Everywhere : obj*(int -> int) -> Constr<'t>
       
-  end
+      abstract To : Constr<'t> -> 't
 
-(*
-type GSumImp() =
-  class
-    inherit GSumR<AList<int>>()
+    type AList<'t> = Cons of 't*AList<'t> | Nil
+    
+    type AListP<'c> =
+      {
+        list : AList<int>
+        meta : 'c
+       }        
 
-    override o.gSum(e : obj) =
+    type EverywhereImp() =
+      class
+        inherit Everywhere<AList<int>>()
 
-      match e with
-        | :? AList<int> as e' ->
-          match e' with
-            | Cons (x,xs) ->
-              o.gSum(L(),{ list = Cons(x,xs) })
-            | Nil -> o.gSum(U(),())
-        | :? AListP as e' ->
-          match e'.list with
-            | Cons (x,xs) ->
-              o.gSum(Prod(), x, (Id(),xs))
-            | Nil ->
-              o.gSum(U(), ())
-        | :? (Id*AList<int>) as e' ->
-          let (_,e'') = e'
-          o.gSum(Id(),e'')
-        | _ -> o.gSum(D(),e)
+        let constrs = FSharpType.GetUnionCases typeof<AList<int>> |> Array.map Reflection.makeTypeConstructor 
 
-  end
-*)
+        let (_,sums) = 
+            constrs 
+            |> Array.fold (fun (constr,mappings) e -> ((fun e' -> (R(constr e') :> Constr<_>)), mappings |> Map.add e constr)) ((fun e -> (L(e) :> Constr<_>)), Map.empty)
+        override o.Everywhere(e:obj,f:int->int) =
+
+            match e with
+                | :? AList<int> as e' ->
+                    let c = constrs |> Array.find (fun c -> c.Matcher e')
+                    \ 
+            
+
+          match e with
+            | :? AList<int> as e' ->
+              Microsoft.FSharp.Reflection.FSharpType.
+            | :? AListP<LConstr> as e' ->
+              match e'.list with
+                | Cons (x,xs) ->
+                  let (c : L<AList<int>>) = LConstr() :> _
+                  o.Everywhere(c,{list=Cons(x,xs);meta=ProdConstr()},f)
+            | :? AListP<ProdConstr> as e' ->
+              match e'.list with
+                | Cons (x,xs) ->
+                  let (c : Prod<AList<int>>) = ProdConstr() :> _
+                  o.Everywhere(c,{list=Cons(x,xs);meta=KConstr()},{list=Cons(x,xs);meta=IdConstr()},f) :> _
+            | :? AListP<KConstr> as e' ->
+              match e'.list with
+                | Cons (x,_) -> o.Everywhere(KConstr(),x,f) :> _
+            | :? AListP<IdConstr> as e' ->
+              match e'.list with
+                | Cons (_,xs) -> o.Everywhere(IdConstr(),xs,f) :> _
+                | Nil ->
+                  let (c : U<AList<int>>) = UConstr() :> _
+                  o.Everywhere(c,(),f) :> _
+            | :? AListP<RConstr> as e' -> o.Everywhere(RConstr(),{list=Nil;meta=IdConstr()},f)
+            | :? AListP<UConstr> -> o.Everywhere(UConstr(),(),f) :> _
+          
+      end
+    
+    (*
+    type GSumImp() =
+      class
+        inherit GSumR<AList<int>>()
+    
+        override o.gSum(e : obj) =
+    
+          match e with
+            | :? AList<int> as e' ->
+              match e' with
+                | Cons (x,xs) ->
+                  o.gSum(L(),{ list = Cons(x,xs) })
+                | Nil -> o.gSum(U(),())
+            | :? AListP as e' ->
+              match e'.list with
+                | Cons (x,xs) ->
+                  o.gSum(Prod(), x, (Id(),xs))
+                | Nil ->
+                  o.gSum(U(), ())
+            | :? (Id*AList<int>) as e' ->
+              let (_,e'') = e'
+              o.gSum(Id(),e'')
+            | _ -> o.gSum(D(),e)
+    
+      end
+    *)
+    
